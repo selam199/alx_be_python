ea6ab06eff28246e7f5a6fb72a2e640282ff8a1f
Selam Mulugeta <selammulugeta994@gmail.com>
2025-01-09 09:38:55 +0300

    Shopping List Manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 def display_menu():
     """Displays the menu options."""
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
